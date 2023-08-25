909643eccc5e2d411732c85571cbff776451d987
Sergey Shpilevskiy <siarhei.shpileuski@gmail.com>
2023-08-25 18:12:00 +0300

    [UPDATE] Move files to sell-token folder

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose --env-file ./.env.local down
-docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'token-lib-cnode')
-docker compose --env-file ./.env.local up -d
